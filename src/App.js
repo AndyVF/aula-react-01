bd0add4548bff974a6afe3716787f8372f73f9ad
Anderson <andersonfinho@hotmail.com>
2021-10-13 18:04:57 -0300

    aula react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aprendendo React</h1>
+      <pre>A coruja por influência da mitologia grega, tanto que Atena, deusa da guerra e da sabedoria,</pre> 
+        <pre>tinha uma coruja como mascote. Os gregos consideravam a noite como o momento do pensamento</pre> 
+        <pre>filosófico e da revelação intelectual e a coruja, por ser uma ave noturna, acabou representando</pre>
+         <pre>essa busca pelo saber.</pre>
+        
+    </div>
+  );
+}
+
+export default App;
